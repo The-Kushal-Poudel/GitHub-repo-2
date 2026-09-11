2ce88afe2df549c602a9c8f60b9e19753d23cfe1
Kushal Poudel <mob.lucid@gmail.com>
2026-09-12 00:30:59 +0545

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-server: {
-    proxy: {
-        '/api': {
-            target: 'http://127.0.0.1:8000',
-            changeOrigin: true,
-        },
-
-        '/storage': {
-            target: 'http://127.0.0.1:8000',
-            changeOrigin: true,
-        },
-    },
-},
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+
+      '/storage': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
